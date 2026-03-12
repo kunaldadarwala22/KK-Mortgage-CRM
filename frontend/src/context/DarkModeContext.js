9e179f50d4bfb99f54ba77620ec89533b49e22c5
kunaldadarwala22 <kunal.dadarwala22@gmail.com>
2026-03-12 16:57:14 +0000

    Create DarkModeContext.js

---
+++ frontend/src/context/DarkModeContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true';
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => setDarkMode(prev => !prev);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => useContext(DarkModeContext);
